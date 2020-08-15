32060b6b20c77392d5c4fcd4a4f1ca952c803c77
Tiago Ikenaga <tiagoikenaga@gmail.com>
2020-08-15 02:17:02 -0300

    atualização da senha

--- SistemaGestaoEstoque/TesteUsuario.java
+++ SistemaGestaoEstoque/TesteUsuario.java
@@ -1,13 +1,14 @@
 
 public class TesteUsuario {
+
 	public static void main(String[] args) {
 		Usuario usuario = new Usuario();
+
 		usuario.nome = "Tiago";
 		usuario.sobrenome = "Ikenaga";
-		usuario.email = "tiago@gmail.com";
-		usuario.senha = "Ikenag@88";
+		usuario.senha = "@rByteT3";
+		usuario.email = "tiago@arbyte.com";
 
-		System.out.println(usuario.autentica("Ikenag@88", "tiago@gmail.com"));
+		System.out.println(usuario.autentica("@rByteT3", "tiago@arbyte.com"));
 	}
-
 }
