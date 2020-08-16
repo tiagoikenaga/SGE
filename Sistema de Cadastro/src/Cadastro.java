39d893dd9e1604035a8db4cc435177e182aaf5ac
Brunna Silva <45864414+BrunnaMaiaradaSilva@users.noreply.github.com>
2020-08-16 20:35:25 -0300

    a função verifica senha deve estar fora da função main :rocket:

--- Sistema de Cadastro/src/Cadastro.java
+++ Sistema de Cadastro/src/Cadastro.java
@@ -11,23 +11,35 @@ public class Cadastro {
 
         String login = nome.charAt(0) + "." + sobrenome;
         System.out.println("Login: " + login);
-        
+
         System.out.println("Digite seu e-mail: ");
         String email = scanner.nextLine();
 
-        boolean cSenha = false;
+        System.out.println("Digite sua senha: ");
+        String senha = scanner.nextLine();
+        // boolean cSenha = false;
 
-        while (cSenha == false) {
-            System.out.println("Digite sua senha: ");
-            String senha = scanner.nextLine();
-            // senha.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[-, !, @])(?=.*[A-Z])")
-            if (senha.contains("@") && senha.length() >= 8) {
-                System.out.println("Cadastro realizado com Sucesso");
-                cSenha = true;
-            } else {
-                System.out.println("Sua senha precisa ter no mínimo 8 caracteres, com pelo menos uma letra maiuscula e numero.Por favor repita o processo.");
-            }
+        // while (cSenha == false) {
+        //     System.out.println("Digite sua senha: ");
+        //     String senha = scanner.nextLine();
+        //     // senha.matches("^(?=.[0-9])(?=.[a-z])(?=.[-, !, @])(?=.[A-Z])(?=.\d).+")
+        //     if (senha.contains("@") && senha.length() >= 8) {
+        //         System.out.println("Cadastro realizado com Sucesso");
+        //         cSenha = true;
+        //     } else {
+        //         System.out.println("Sua senha precisa ter no mínimo 8 caracteres, com pelo menos uma letra maiuscula e numero.Por favor repita o processo.");
+        //     }
+
+        scanner.close();
+    }
+    
+    boolean verificaSenha(String senha){
+        if (senha.matches("(?=.[0-9])(?=.[a-z])(?=.[-, !, @])(?=.*[A-Z])") && senha.length() >= 8) {
+            System.out.println("Cadastro realizado com Sucesso");
+            return true;
+        } else {
+            System.out.println("Sua senha precisa ter no mínimo 8 caracteres, com pelo menos uma letra maiuscula e numero.Por favor repita o processo.");
+            return false;
         }
-        scanner.close();   
     }
 }
