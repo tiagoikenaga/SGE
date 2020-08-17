6fd15d9a612657e085190b915c94accf92855e39
TiagoIkenaga <tiagoikenaga@gmail.com>
2020-08-17 17:05:28 -0300

    Update Cadastro.java
    consegui acrescentar o sistema de login

--- Sistema de Cadastro/src/Cadastro.java
+++ Sistema de Cadastro/src/Cadastro.java
@@ -1,31 +1,72 @@
 import java.util.Scanner;
-
 public class Cadastro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o seu Nome: ");
-        String nome = scanner.nextLine();
-        System.out.println("Digite o seu Sobrenome: ");
-        String sobrenome = scanner.nextLine();
+        boolean cadUser = false;
+        boolean cadEmail = false;
+        boolean cadSenha = false;
 
-        String login = nome.charAt(0) + "." + sobrenome;
-        System.out.println("Login: " + login);
+        String nome = null;
+        String sobrenome = null;
+        String email = null;
+        String senha = null;
+        String login = null;
         
-        System.out.println("Digite seu e-mail: ");
-        String email = scanner.nextLine();
-
-        boolean cSenha = false;
-
-        while (cSenha == false) {
-            System.out.println("Digite sua senha: ");
-            String senha = scanner.nextLine();
-            // senha.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[-, !, @])(?=.*[A-Z])")
-            if (senha.contains("@") && senha.length() >= 8) {
-                System.out.println("Cadastro realizado com Sucesso");
-                cSenha = true;
+        while (cadUser == false) {
+            System.out.print("Digite o seu Nome: ");
+            nome = scanner.nextLine();
+            System.out.print("Digite o seu Sobrenome: ");
+            sobrenome = scanner.nextLine();
+            
+            if ((nome.length() != 0) && (sobrenome.length() != 0)) {
+                System.out.println("Login cadastrado");
+                cadUser = true;
             } else {
-                System.out.println("Sua senha precisa ter no mínimo 8 caracteres, com pelo menos uma letra maiuscula e numero.Por favor repita o processo.");
+                System.out.println("Insira o nome e sobrenome para cadastro de login.");
+            }
+         }
+        while (cadEmail == false) {
+            System.out.print("Digite seu e-mail: ");
+            email = scanner.nextLine();
+
+            if (email.endsWith(".com")) {
+                System.out.println("email cadastrado");
+                cadEmail = true;                
+            } else {
+                System.out.println("email inválido");
+            }            
+        }
+        while (cadSenha == false) {
+            System.out.print("Digite sua senha: ");
+            senha = scanner.nextLine();
+            // senha.contains("@") && senha.length() >= 8 && email.length()>0
+            if (senha.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[-, !, @])(?=.*[A-Z])(?=.*\\d).+") && senha.length() >= 8) {
+                System.out.println("Senha cadastrada");
+                cadSenha = true;
+            } else {
+                System.out.println("Sua senha precisa ter no mínimo 8 caracteres, com pelo menos 1 letra maiuscula, 1 numero, e 1 desses caracteres (#, !, @). Por favor repita o processo.");
+            }
+        }
+        if (cadUser == true && cadEmail == true && cadSenha == true) {
+            System.out.println("Cadastro realizado");            
+            login = nome.charAt(0) + "." + sobrenome;
+            System.out.println("Login: " + login);
+            System.out.println("Senha: "+ senha);
+        }
+        boolean validar = false;
+        while (validar == false) {
+            System.out.print("Insira o seu login: ");
+            String vLogin = scanner.nextLine();
+
+            System.out.print("Insira sua senha: ");
+            String vSenha = scanner.nextLine();
+
+            if (vLogin.equals(login) && vSenha.equals(senha)) {
+                System.out.println("Login realizado com Sucesso");
+                validar = true;               
+            } else {
+                System.out.println("Login ou Senha inválidos");                                
             }
         }
         scanner.close();   
